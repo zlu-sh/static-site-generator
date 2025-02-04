40fe6539cee01b92e7e12d41f8215293408c295c
zlu-sh <dev@zlu.sh>
2025-02-04 13:40:38 -0600

    Add unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_type_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_no_url(self):
+        node = TextNode("this is a text node", TextType.BOLD)
+        self.assertIsNone(node.url)
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -18,7 +18,10 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        return self == other
+        return (
+                self.text == other.text and
+                self.text_type == other.text_type and
+                self.url == other.url )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url}"
