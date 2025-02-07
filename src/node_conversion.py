f3dd46bef7c71cbbaeff65aaf980fcaa94131211
zlu-sh <dev@zlu.sh>
2025-02-07 16:46:22 -0600

    Add defensive type check and invalid case

--- src/node_conversion.py
+++ src/node_conversion.py
@@ -2,6 +2,8 @@ from textnode import TextType
 from leafnode import LeafNode
 
 def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise Exception("node text_type must be of TextType enum")
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None,text_node.text)
     elif text_node.text_type == TextType.BOLD:
@@ -14,3 +16,5 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
     elif text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": f"{text_node.url}", "alt": f"{text_node.text}"})
+    else:
+        raise Exception("text_type is not a valid type")
