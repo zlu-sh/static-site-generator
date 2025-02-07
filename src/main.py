855d921ec5b525ef5c29a5dd4785b5c65ad09604
zlu-sh <dev@zlu.sh>
2025-02-07 16:08:02 -0600

    Write code to test functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from leafnode import LeafNode
+from parentnode import ParentNode
 
 def main():
     print(TextNode("some_shit", TextType.BOLD, "hhttttt"))
@@ -10,9 +11,21 @@ def main():
 
     node1 = LeafNode("p", "Paragraph", {"style": "color:blue;size:16;"})
     node2 = LeafNode("a", "Click this", {"href": "https://google.com", "style": "color:red;"})
+    node3 = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+
     html1 = node1.to_html()
     html2 = node2.to_html()
+    html3 = node3.to_html()
     print(html1)
     print(html2)
+    print(html3)
 
 main()
