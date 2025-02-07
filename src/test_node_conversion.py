412a8dd5a4abfef46ecae79f777c70ba57c47ef9
zlu-sh <dev@zlu.sh>
2025-02-07 17:50:16 -0600

    Add more unit tests

--- src/test_node_conversion.py
+++ src/test_node_conversion.py
@@ -3,33 +3,50 @@ from node_conversion import text_node_to_html_node
 from textnode import TextNode, TextType
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
+
     def test_empty_text(self):
         node = TextNode("", TextType.TEXT) # Create a node with empty text
         result = text_node_to_html_node(node) # Turn that text node into an html node
         self.assertEqual(result.value, "") # Check to see if the text of html node is empty text
 
-    def test_is_bold(self):
-        node = TextNode("Text", TextType.BOLD)
+    def test_bold_node(self):
+        node = TextNode("text", TextType.BOLD)
         result = text_node_to_html_node(node)
         self.assertEqual(result.tag, "b")
+        self.assertEqual(result.value, "text")
 
-    def test_is_italic(self):
-        node = TextNode("Text", TextType.ITALIC)
+    def test_italic_node(self):
+        node = TextNode("text", TextType.ITALIC)
         result = text_node_to_html_node(node)
         self.assertEqual(result.tag, "i")
+        self.assertEqual(result.value, "text")
 
-    def test_is_code(self):
-        node = TextNode("Text", TextType.CODE)
+    def test_code_node(self):
+        node = TextNode("text", TextType.CODE)
         result = text_node_to_html_node(node)
         self.assertEqual(result.tag, "code")
+        self.assertEqual(result.value, "text")
 
-    def test_is_link(self):
-        node = TextNode("Text", TextType.LINK, "https://zlu.sh/")
+    def test_link_node(self):
+        url = "https://zlu.sh"
+        node = TextNode("text", TextType.LINK, url)
         result = text_node_to_html_node(node)
-        self.assertEqual(result.tag, "aa")
 
-    def test_link_has_url(self):
-        url = "https://zlu.sh/"
-        node = TextNode("Text", TextType.LINK, url)
-        result = text_node_to_html_node(node)
+        self.assertEqual(result.tag, "a")
+        self.assertEqual(result.value, "text")
         self.assertEqual(result.props["href"], url)
+
+    def test_image_node(self):
+        alt_text = "alt text"
+        src_url = "./img.png"
+        node = TextNode(alt_text, TextType.IMAGE, src_url)
+        result = text_node_to_html_node(node)
+
+        self.assertEqual(result.tag, "img")
+        self.assertEqual(result.props["src"], src_url)
+        self.assertEqual(result.props["alt"], alt_text)
+
+    def test_invalid_text_type(self):
+        node = TextNode("text", "not_a_valid_type")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
