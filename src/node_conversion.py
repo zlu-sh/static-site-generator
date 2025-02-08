60c955a8330664b33ae1776e879e8ae913a7a211
zlu-sh <dev@zlu.sh>
2025-02-07 19:12:02 -0600

    Remove unncessary f-strings

--- src/node_conversion.py
+++ src/node_conversion.py
@@ -13,8 +13,8 @@ def text_node_to_html_node(text_node):
     elif text_node.text_type == TextType.CODE:
         return LeafNode("code", text_node.text)
     elif text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     elif text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", "", {"src": f"{text_node.url}", "alt": f"{text_node.text}"})
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     else:
         raise Exception("text_type is not a valid type")
