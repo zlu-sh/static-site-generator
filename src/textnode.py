5ba8759c644da6a33533f0a18ca4fd1232a80263
zlu-sh <dev@zlu.sh>
2025-02-07 18:04:58 -0600

    Remove invalid file type check

--- src/textnode.py
+++ src/textnode.py
@@ -11,8 +11,6 @@ class TextType(Enum):
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
-        if not isinstance(text_type, TextType):
-            raise ValueError("text_type must be a TextType enum")
         self.text = text
         self.text_type = text_type
         self.url = url
